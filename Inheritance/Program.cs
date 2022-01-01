7f46185860f5b16b735917e953aa6cad0cc239c6
Bálint Csertán <csertanb@hotmail.com>
2022-01-01 11:55:39 +0100

    Add project files.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Inheritance.Radio radio = new Inheritance.Radio(false, "Sanyo");
+
+radio.ListenRadio();
+radio.SwitchOn();
+radio.ListenRadio();
+
---
+++ Inheritance/Radio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Radio : ElectricalDevice
+    {
+        public Radio(bool isOn, string brand) : base(isOn, brand)
+        { 
+        }
+
+        public void ListenRadio()
+        {
+            if (IsOn)
+            {
+                Console.WriteLine("Listening to radio.");
+            }
+            else
+            {
+                Console.WriteLine("Radio is off, please switch it on first!");
+            }
+        }
+    }
+}
